981605d973b39409945c6d2b31671852847f24f7
xiaotianxt <tianyp@pku.edu.cn>
2022-10-16 20:59:09 +0800

    feat(debugger): add debugger

---
+++ debugger.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         Debugger
+// @description  A native file debugger for PKU-Thesis-Downloader,
+//               replace the file url at line 10, and create a new
+//               user script in tampermonkey.
+// @author       xiaotianxt
+// @match        http://162.105.134.201/pdfindex.jsp?fid=*
+// @match        https://drm.lib.pku.edu.cn/pdfindex.jsp?fid=*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=pku.edu.cn
+// @require      https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js
+// @require      https://cdnjs.cloudflare.com/ajax/libs/notify/0.4.2/notify.min.js
+// @require      file:///Users/tian/Development/PKU-Thesis-Download/main.js
+// @license      GNU GPLv3
+// ==/UserScript==
