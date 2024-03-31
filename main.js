a3c772fef67495d3bbc077d0d9d627bfed10b17b
xiaotianxt <tianyp@pku.edu.cn>
2024-03-31 19:02:44 +0800

    fix: url match pattern

--- main.js
+++ main.js
@@ -3,11 +3,11 @@
 // @namespace    https://greasyfork.org/zh-CN/scripts/442310-pku-thesis-download
 // @supportURL   https://github.com/xiaotianxt/PKU-Thesis-Download
 // @homepageURL  https://github.com/xiaotianxt/PKU-Thesis-Download
-// @version      1.1
+// @version      1.1.1
 // @description  北大论文平台下载工具，请勿传播下载的文件，否则后果自负。
 // @author       xiaotianxt
-// @match        http://162.105.134.201/pdfindex.jsp?fid=*
-// @match        https://drm.lib.pku.edu.cn/pdfindex.jsp?fid=*
+// @match        http://162.105.134.201/pdfindex*
+// @match        https://drm.lib.pku.edu.cn/pdfindex*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=pku.edu.cn
 // @require      https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js
 // @require      https://cdnjs.cloudflare.com/ajax/libs/notify/0.4.2/notify.min.js
